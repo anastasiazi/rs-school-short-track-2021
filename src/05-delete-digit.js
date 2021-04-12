ab0213ed2efa9b2d4cea61509c8270d1b2dfa6c0
anastasiazi <anastasiiiazi@gmail.com>
2021-04-12 13:46:53 +0300

    feat: delete-digit function was implemented

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,11 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const array = Array.from(n.toString(10)).map((x) => +x);
+  const i = array.indexOf(Math.min(...array));
+  array.splice(i, 1);
+  return +array.join('');
 }
 
 module.exports = deleteDigit;
