369a4d00bd53ade1a77aeb0af658a6eea509c2bf
anastasiazi <anastasiiiazi@gmail.com>
2021-04-12 19:50:12 +0300

    feat: encode-line function was implemented

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 0;
+  let prevLetter;
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    const letter = str.charAt(i);
+    if (prevLetter === undefined) {
+      prevLetter = letter;
+      count++;
+    } else if (prevLetter !== letter) {
+      result = result.concat(count > 1 ? count : '').concat(prevLetter);
+      count = 1;
+      prevLetter = letter;
+    } else {
+      count++;
+    }
+    if (i === str.length - 1) {
+      result = result.concat(count > 1 ? count : '').concat(prevLetter);
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
